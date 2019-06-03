11f1eea554d32d4191cb447e4da33fe8b659e790
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2019-06-03 11:10:25 -0700

    add code to js and html

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $("form#match").submit(function(event) {
+    var age = parseInt($("input#age").val());
+    var gender = $("select#gender").val();
+
+    if (age) {
+    if (gender === 'male' && age < 26) {
+        quote += "Zach Morris";
+      }
+
+      $("#perfect-match").empty().append(quote);
+      $("#quote").show();
+    } else {
+      alert('Please enter your age.');
+    }
+
+    event.preventDefault();
+  });
+});
