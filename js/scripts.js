314a4809641777c1af588d0133a82ff3a03958a0
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2019-06-04 10:28:32 -0700

    fix scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
   $("form#match").submit(function(event) {
+    var quote = '';
     var age = parseInt($("input#age").val());
+
     var gender = $("select#gender").val();
 
     if (age) {
-    if (gender === 'female' && age < 26) {
-        quote += "Zach Morris";
-      }
-
+    if (gender === 'female' && age <= 18) {
+        quote = "Zach Morris";
+    } else { 
+      quote = "Jerry Seinfield";
+    }
+      console.log(quote);
       $("#perfect-match").empty().append(quote);
       $("#quote").show();
     } else {
@@ -17,3 +21,4 @@ $(document).ready(function() {
     event.preventDefault();
   });
 });
+
